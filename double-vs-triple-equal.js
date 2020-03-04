f5814aa94a50bad77ef9443358dc44af6fbd2b11
ForhadkhanAJ <forhadkhan2255@gmail.com>
2020-03-04 22:30:21 +0600

    double vs triple equal, complete, very important

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,99 @@
+//15.4 == VS ===, IMPLICIT CONVERSION
+// //1(A)
+// const first = 2;
+
+// const second = 3;
+// if(first == second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// //1(A)
+// const first = 2;
+
+// const second = 2;
+// if(first == second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// //1(A)
+// const first = 2;
+
+// const second = "2";
+// if(first == second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// //1(A)
+// const first = 2;
+
+// const second = "2";
+// if(first === second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// //1(A)
+// const first = 2;
+
+// const second = true;
+// if(first == second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// //1(A)
+// const first = 1;
+
+// const second = true;
+// if(first == second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// //1(A)
+// const first = 1;
+
+// const second = false;
+// if(first == second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+// //1(A)
+// const first = 0;
+
+// const second = false;
+// if(first == second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+//1(A)
+const first = 0;
+
+const second = false;
+if(first === second){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
